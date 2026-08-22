04ab10630ca5205e5dba62400a950841a50fed79
MrLawli3t <34941773+MrLawli3t@users.noreply.github.com>
2026-08-22 23:13:37 +0200

    added nvim-notify and noice for cool stuff

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,5 +2,11 @@ local map = vim.keymap.set
 
 vim.g.mapleader = " "
 
-map("n", "<leader>cd", vim.cmd.Ex)
+map("n", "<leader>ee", vim.cmd.Ex)
 map("n", "<leader>w", vim.cmd.w)
+map("n", "<leader>cd", function ()
+	local dir = vim.fn.expand("%:p:h")
+	vim.cmd("cd " .. vim.fn.expand(dir))
+	vim.cmd("pwd")
+end, { desc = "Change CWD to current file directory"}
+)
