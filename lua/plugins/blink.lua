00a31420ec364cef1d272fc6d4d6852d1403f468
MrLawli3t <34941773+MrLawli3t@users.noreply.github.com>
2026-08-25 19:27:28 +0200

    plugin stuff

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,29 +1,32 @@
 return {
-  {
-    'Saghen/blink.cmp',
-    version = '*',
-    opts = {
-      keymap = { preset = 'default' },
-      sources = {
-        default = { 'lsp', 'path', 'snippets', 'buffer' },
-      },
-    },
-  },
-  {
-    'neovim/nvim-lspconfig',
-    dependencies = { 'Saghen/blink.cmp' },
-    config = function()
-      -- 1. Grab autocomplete capabilities from blink
-      local capabilities = require('blink.cmp').get_lsp_capabilities()
-      
-      -- 2. Inject capabilities into the native 'marksman' config
-      --    This safely merges your settings with nvim-lspconfig's defaults
-      vim.lsp.config('marksman', {
-        capabilities = capabilities,
-      })
+	{
+		'Saghen/blink.cmp',
+		version = '*',
+		priority = 1000,
+		opts = {
+			keymap = {
+				preset = 'default',
+			},
+		sources = {
+			default = { 'lsp', 'path', 'snippets', 'buffer' },
+		},
+	},
+},
+{
+	'neovim/nvim-lspconfig',
+	dependencies = { 'Saghen/blink.cmp' },
+	config = function()
+		-- 1. Grab autocomplete capabilities from blink
+		local capabilities = require('blink.cmp').get_lsp_capabilities()
 
-      -- 3. Natively enable the language server
-      vim.lsp.enable('marksman')
-    end
-  }
+		-- 2. Inject capabilities into the native 'marksman' config
+		--    This safely merges your settings with nvim-lspconfig's defaults
+		vim.lsp.config('marksman', {
+			capabilities = capabilities,
+		})
+
+		-- 3. Natively enable the language server
+		vim.lsp.enable('marksman')
+	end
+}
 }
